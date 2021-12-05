85485065c7aba02d7846e743d588aeece5613fb9
Andreas Trawoeger <atrawog@dorgeln.org>
2021-12-05 18:19:27 +0000

    Add terraform config

---
+++ instance.tf
@@ -0,0 +1,15 @@
+resource "openstack_compute_instance_v2" "tf1" {
+  name        = "tf1" # Instance name
+  provider    = openstack.ovh  # Provider name
+  image_name  = "Ubuntu 20.04" # Image name
+  flavor_name = "s1-2" # Instance type name
+  metadata = {
+     ansible_group = "terrabuilder"
+     ansible_user = "ubuntu"
+   }
+   # Name of openstack_compute_keypair_v2 resource named keypair_test
+   key_pair    = openstack_compute_keypair_v2.terrabuilder.name
+   network {
+     name      = "Ext-Net" # Adds the network component to reach your instance
+   }
+ }
---
+++ provider_ovh.tf
@@ -0,0 +1,7 @@
+provider "openstack" {
+  auth_url    = "https://auth.cloud.ovh.net/v3/" # Authentication URL
+  domain_name = "default" # Domain name - Always at 'default' for OVHcloud
+  alias       = "ovh" # An alias
+
+}
+
