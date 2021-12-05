2d7e731e4e2fa012a55a1b566849b6f089df0dc5
Andreas Trawoeger <atrawog@dorgeln.org>
2021-12-05 21:36:26 +0000

    Add tf2 instance

--- instance.tf
+++ instance.tf
@@ -12,3 +12,18 @@ resource "openstack_compute_instance_v2" "tf1" {
     name = "Ext-Net" # Adds the network component to reach your instance
   }
 }
+
+resource "openstack_compute_instance_v2" "tf2" {
+  name        = "tf2"          # Instance name
+  provider    = openstack.ovh  # Provider name
+  image_name  = "Ubuntu 20.04" # Image name
+  flavor_name = "s1-2"         # Instance type name
+  metadata = {
+    ansible_group = "terrabuilder"
+    ansible_user  = "ubuntu"
+  }
+  # Name of openstack_compute_keypair_v2 resource named keypair_test
+  network {
+    name = "Ext-Net" # Adds the network component to reach your instance
+  }
+}
